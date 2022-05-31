4d6eac015b4d4fa063ff11b332d5125962f28c72
Fili_vw23@student.itstep.org <Fili_vw23@student.itstep.org>
2022-05-31 16:35:05 +0300

    Добавьте файлы проекта.

---
+++ FoxHunt/Field.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoxHunt
+{
+    class Field
+    {
+        public int[,] FieldInfo { get; set; }
+
+
+
+
+        //Живых лисиц
+        public int FoxLiving { get; set; }
+
+        //патронов осталось
+        public int Ammo { get; set; }
+
+        //Кординаты для поиска количества лисиц (-1 по умолчанию)
+        private int oldX = -1;
+        private int oldY = -1;
+
+
+        public Field()
+        {
+            this.FieldInfo = new int[Program.Size, Program.Size];
+            this.FoxLiving = Program.Fox;
+            this.Ammo = Program.Ammo;
+        }
+
+        //Выводит поле (просто визуализация)
+        public void showField()
+        {
+            for (int i = 0; i < Program.Size; i++)
+            {
+                for (int j = 0; j < Program.Size; j++)
+                {
+                    Console.Write("■  ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        //Debug mode для проверки корректности
+        public void showFieldWithFox()
+        {
+            for (int i = 0; i < Program.Size; i++)
+            {
+                for (int j = 0; j < Program.Size; j++)
+                {
+                    Console.Write($"{FieldInfo[i, j]}  ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        //Создаём лисиц
+        public void spawnFox()
+        {
+            preGenerateField();
+
+            Random rand = new Random();
+            for (int i = 0; i < Program.Fox; i++)
+            {
+                int iCord = rand.Next(0, 5);
+                int jCord = rand.Next(0, 5);
+
+                //Добавляем лису
+                this.FieldInfo[iCord, jCord]++;
+            }
+        }
+
+        //Подготовка массива к добавлению лисиц
+        private void preGenerateField()
+        {
+            for (int i = 0; i < Program.Size; i++)
+            {
+                for (int j = 0; j < Program.Size; j++)
+                {
+                    //Выставляем значения массиву 
+                    //Значения массива будут показывать количество лисиц
+                    this.FieldInfo[i, j] = 0;
+                }
+            }
+        }
+
+        //Метод для проверки на убийство
+        public bool tryKillFox(int x, int y)
+        {
+            x--;
+            y--;
+            try
+            {
+                if (this.FieldInfo[y, x] > 0)
+                {
+                    //Уменьшаем количество живых лисиц
+                    this.FoxLiving--;
+
+                    //Обновляем информацию о поле
+                    this.FieldInfo[y, x]--;
+                    
+                    //Тратим патрон
+                    this.Ammo--;
+                    return true;
+                }
+                //Сохраняем информацию о координатах
+                this.oldX = x;
+                this.oldY = y;
+
+                //Тратим патрон
+                this.Ammo--;
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                //Проверка на "дурака"
+                Console.WriteLine("Введены некорректные координаты");
+            }
+            return false;
+        }
+
+        //проверка лисиц по горизонтали и вертикали от главной лисы
+        public int getFoxInRow()
+        {
+
+            if (oldX == -1)
+            {
+                return 0;
+            }
+
+            int fox = 0;
+            for (int i = 0; i < Program.Size; i++)
+            {
+                for (int j = 0; j < Program.Size; j++)
+                {
+                    if (i == oldY || j == oldX)
+                    {
+                        if (this.FieldInfo[i, j] != 0)
+                        {
+                            fox++;
+                        }
+                    }
+                }
+            }
+            return fox;
+        }
+    }
+}
---
+++ FoxHunt/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace FoxHunt
+{
+    class Program
+    {
+
+        //Debug mode для удобной проверки корректности
+        public static bool Debug = false;
+
+        //Размер поля 
+        public static int Size = 5;
+
+        //Количество лисиц
+        public static int Fox = 5;
+
+        //Количество пуль
+        public static int Ammo = 10;
+
+
+
+
+        static void Main(string[] args)
+        {
+            Field GameField = new Field();
+            GameField.spawnFox();
+
+            do
+            {
+                if (Debug == true)
+                {
+                    GameField.showFieldWithFox();
+                }
+                else
+                {
+                    GameField.showField();
+                }
+                Console.WriteLine($"Патроны: {GameField.Ammo}");
+                Console.WriteLine($"Лисицы рядом: {GameField.getFoxInRow()}");
+                Console.WriteLine($"Лисиц всего: {GameField.FoxLiving}");
+
+                Console.WriteLine("\nВведите X: ");
+                int x = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите Y: ");
+                int y = int.Parse(Console.ReadLine());
+
+                //Попытка убийства, выделено цветами для более простого понимания 
+                Console.Clear();
+                if (GameField.tryKillFox(x, y))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Вы попали!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Вы не попали!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                //Проверка на победу или проигрыш
+            } while (GameField.Ammo != 0 && GameField.FoxLiving != 0);
+
+
+            Console.Clear();
+            if (GameField.FoxLiving == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Вы выиграли!");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Вы проиграли!");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}
